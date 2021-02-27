f71b03ae0424c90e978bcf9c21ed0b4c62c34a36
Kelvis Borges <kelvisbcc@gmail.com>
2021-02-27 12:05:08 -0300

    Ajuste font e styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import Head from "next/head";
 import React from "react";
+import { CompletedChallenges } from "../components/CompletedChalleges";
+import { Countdown } from "../components/CountDown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 
 import styles from '../styles/pages/Home.module.css';
 
-export default function Home() {
+export default function Home() { 
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
 
         <div> 
