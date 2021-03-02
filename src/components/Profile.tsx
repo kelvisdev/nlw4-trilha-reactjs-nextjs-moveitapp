b078214d7a92b3ed654898d41646ffa33025e5f0
Kelvis Borges <kelvisbcc@gmail.com>
2021-03-01 23:17:10 -0300

    Ajuste componente Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,18 @@
-import styles from '../styles/components/Profile.module.css';
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/kelvisdev.png" alt="Kelvis Borges" />
       <div>
         <strong>Kelvis Borges</strong>
         <p>
-          <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
         </p>
       </div>
     </div>
